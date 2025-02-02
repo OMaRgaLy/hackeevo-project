08180825b617c579189b78213a333fe8102283f4
Omargaly Bitebayev <omargaly.rs@gmail.com>
2025-02-02 05:42:17 +0500

    create structure

---
+++ src/config/database.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Config;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private static $instance = null;
+
+    public static function getConnection()
+    {
+        if (self::$instance === null) {
+            try {
+                $host = "localhost";
+                $db = 'hackeevo';
+                $user = 'root';
+                $pass = '1465';
+                $charset = 'utf8mb4';
+
+                $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+                $options = [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_EMULATE_PREPARES => false,
+                ];
+
+                self::$instance = new PDO($dsn, $user, $pass, $options);
+            } catch (PDOException $e) {
+                throw new PDOException($e->getMessage(), (int)$e->getCode());
+            }
+        }
+
+        return self::$instance;
+    }
+}
